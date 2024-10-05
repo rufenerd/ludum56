0a80053ed47d6084377494afb3c25b4818c06a3d
Dan Rufener <daniel.rufener@gmail.com>
2024-10-05 16:00:07 -0700

    Expose demand

--- src/Stats.js
+++ src/Stats.js
@@ -2,11 +2,15 @@
 function Stats(props) {
     return (
         <div className='stats'>
-            <div>
-                FOOD: {props.food}
-            </div>
+
             <div>
                 POPULATION: {props.population.length}
+            </div>
+            <div>
+                FOOD SUPPLY: {props.food}
+            </div>
+            <div>
+                FOOD DEMAND: {props.population.reduce((m, a) => m + a.foodRequirement, 0)}
             </div>
         </div>
     );
