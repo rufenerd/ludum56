16183a2810f871db86c31bbc2311c683ad62e947
Dan Rufener <daniel.rufener@gmail.com>
2024-10-05 16:07:39 -0700

    Hand style

--- src/Hand.js
+++ src/Hand.js
@@ -3,7 +3,6 @@ import GooberGroup from "./GooberGroup";
 function Hand(props) {
     return (
         <div className="hand">
-            HAND:
             <GooberGroup goobers={props.hand} onClick={(x) => props.onClick(x)} />
         </div>
     );
