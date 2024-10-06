62543b24df1fe1fd96601aa0d808168ff79d7a08
Dan Rufener <daniel.rufener@gmail.com>
2024-10-05 19:10:37 -0700

    hack to end turn if nothing to do

--- src/PlayArea.js
+++ src/PlayArea.js
@@ -5,6 +5,11 @@ import TeamBar from './TeamBar';
 function PlayArea(props) {
     const { food, hand, population, team, onAddGooberToTeam, onStayClick, onBreedClick, onExpeditionClick, onEndTurn } = props
     const stillInHand = hand.filter(x => !team.includes(x))
+
+    if (hand.length == 0 && team.length == 0) {
+        onEndTurn()
+    }
+
     return (
         <div className="play-area">
             <Stats food={food} population={population} />
