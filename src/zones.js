68653baa3a6aaea9428582cf9b9b9772955bfa68
Dan Rufener <daniel.rufener@gmail.com>
2024-10-06 16:24:29 -0700

    Initial zone placement

--- src/zones.js
+++ src/zones.js
@@ -1,64 +1,90 @@
 export const zones = [
     {
-        name: "zone1-1",
+        name: "Cheep-o Animal Feed",
         risk: 1,
         bounty: 10,
         remaining: 100,
         unlocked: true,
         room: "start",
-        x: 5150,
-        y: 0
+        x: 9285,
+        y: 1316
     },
     {
-        name: "zone1-2",
+        name: "Cabinet",
         risk: 2,
         bounty: 20,
         remaining: 200,
         unlocked: false,
         canUnlock: (team) => team.map(x => x.klass).includes("opener"),
         room: "start",
-        requires: "opener"
+        requires: "opener",
+        x: 9052,
+        y: 1146
     },
     {
         name: "Vent",
-        risk: 3,
+        risk: 2,
         bounty: 100,
         remaining: 100,
         unlocked: false,
         canUnlock: (team) => team.map(x => x.klass).includes("explorer"),
         room: "start",
-        unlocksRoom: 2,
-        x: 603,
-        y: 152,
+        unlocksRoom: "breakroom",
+        x: 8881,
+        y: 233,
         requires: "explorer"
     },
     {
-        name: "zone-2-1",
-        risk: "start",
+        name: "Containment Room Door",
+        risk: 2,
+        bounty: 100,
+        remaining: 100,
+        unlocked: false,
+        canUnlock: (team) => team.map(x => x.klass).includes("explorer"),
+        room: "start",
+        unlocksRoom: "hall",
+        x: 8484,
+        y: 985,
+        requires: "explorer"
+    },
+    {
+        name: "Food Cabinet",
+        risk: 1,
         bounty: 10,
         remaining: 100,
         unlocked: true,
-        room: 2
+        room: "breakroom",
+        x: 6662,
+        y: 584
     },
     {
-        name: "zone2-2",
+        name: "Microwave",
         risk: 2,
-        bounty: 0,
-        remaining: 0,
-        unlocked: false,
-        canUnlock: (team) => team.map(x => x.klass).includes("explorer"),
-        room: 2,
-        unlocksRoom: 3,
-        requires: "explorer"
+        bounty: 20,
+        remaining: 200,
+        unlocked: true,
+        room: "breakroom",
+        x: 6670,
+        y: 949
     },
     {
-        name: "zone3-1",
-        risk: 3,
-        bounty: 400,
-        remaining: 1000,
-        unlocked: false,
-        canUnlock: (team) => team.map(x => x.klass).includes("explorer") && team.map(x => x.klass).includes("opener"),
-        room: 3,
-        requires: "explorer and opener"
+        name: "Table",
+        risk: 1,
+        bounty: 20,
+        remaining: 60,
+        unlocked: true,
+        room: "breakroom",
+        x: 7050,
+        y: 983
+    },
+    {
+        name: "Mouse Trap Bait",
+        risk: 5,
+        bounty: 100,
+        remaining: 100,
+        unlocked: true,
+        room: "breakroom",
+        x: 6838,
+        y: 1334
     }
 ]
