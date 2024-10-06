d1daa1e37e83be4a770cd5fa768e89dc82cee3ff
Dan Rufener <daniel.rufener@gmail.com>
2024-10-05 18:57:37 -0700

    style

--- src/TeamBar.js
+++ src/TeamBar.js
@@ -2,13 +2,16 @@ import GooberGroup from './GooberGroup';
 
 function TeamBar(props) {
     const { team, onGooberClick, onStayClick, onBreedClick, onExpeditionClick, onEndTurn } = props
+
+    const noTeam = team.length == 0 ? "no-team" : ""
+
     return (
         <div className="team-bar">
             <GooberGroup goobers={team} onClick={onGooberClick} />
             <div className="actions">
-                {team.length > 0 && <button onClick={onStayClick}>CHILL</button>}
-                {team.length > 0 && <button onClick={onBreedClick}>BREED</button>}
-                {team.length > 0 && <button onClick={onExpeditionClick}>ADVENTURE</button>}
+                {<button className={noTeam} onClick={onStayClick}>CHILL</button>}
+                {<button className={noTeam} onClick={onBreedClick}>BREED</button>}
+                {<button className={noTeam} onClick={onExpeditionClick}>ADVENTURE</button>}
                 <button onClick={onEndTurn}>END TURN</button>
             </div>
         </div>
