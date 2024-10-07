2d21b6842403771f00a5a5d1fe0c94c852b33d34
Dan Rufener <daniel.rufener@gmail.com>
2024-10-07 10:41:49 -0700

    Update tutorial for reproducing

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -10,12 +10,20 @@ function Tutorial(props) {
             img: 'assets/goober.png',
         },
         {
-            text: 'Some tiny and helpless creature, no doubt. The poor thing, trapped in this lab, with only a small box and a few others of its kind of enrichment. You can tell that they yearn for the ourdoors...',
+            text: 'Some tiny and helpless creature, no doubt. The poor thing, trapped in this lab, with only a small box and a few others of its kind for enrichment. You can tell that they yearn for the outdoors...',
             img: 'assets/goober.png',
         },
         {
             text: 'In this game you\'ll lead the goobers to freedom from their laboratory. You\'ll control the population of goobers, sending them on expeditions in this unknown environment to gather food and explore new locations. But be careful! Danger lurks around every corner.'
         },
+        {
+            text: 'They won\'t be able to make it on their own, but they can reproduce to get more help.',
+            img: 'assets/reproduce_tutorial.png',
+        },
+        {
+            text: 'Goobers can combine to produce new types of offspring. Experiment with pairing different combinations!',
+            img: 'assets/combination_tutorial.png',
+        }
 
     ]} />
 }
